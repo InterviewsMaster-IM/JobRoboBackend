da78abc7ebb2d1a5f9fa3538bca8d621fcc70d4c
tarunkota <tarunkota4@gmail.com>
2024-01-03 17:28:48 +0530

    Better responses Added better prompt Increased token validity added api for testing resume queries for the test page

--- resumes/urls.py
+++ resumes/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import resume_upload, start_parse_resume_task, check_parse_resume_task_status
+from .views import resume_upload, start_parse_resume_task, check_parse_resume_task_status, resume_query
 
 urlpatterns = [
     path('upload/', resume_upload, name='file-upload'),
-    path('start-task/', start_parse_resume_task, name='start-parse-resume-task'),
-    path('check-task/<str:task_id>/', check_parse_resume_task_status, name='check-parse-resume-task-status'),
+    path('start-task/', start_parse_resume_task,
+         name='start-parse-resume-task'),
+    path('check-task/<str:task_id>/', check_parse_resume_task_status,
+         name='check-parse-resume-task-status'),
+    path('qa/', resume_query, name='resume-query')
 ]
