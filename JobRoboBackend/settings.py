bb581ba97b2b7778e03cd051579bf3a33765a2bc
tarunkota <tarunkota4@gmail.com>
2024-01-04 16:24:57 +0530

    changed ip for web hosting

--- JobRoboBackend/settings.py
+++ JobRoboBackend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-8=!ds1)(^$*jw645^#j!l+^crzgr-tnn4@oflf(j48u^l@oelz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "18.217.21.55"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "8.222.209.106"]
 
 
 # Application definition
