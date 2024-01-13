22762bf3d1c6680bdbe749281eec0e13e116522a
tarunkota <tarunkota4@gmail.com>
2024-01-13 19:15:00 +0530

    bug fix

--- campaigns/urls.py
+++ campaigns/urls.py
@@ -4,6 +4,6 @@ from .views import create_campaign, update_campaign
 
 urlpatterns = [
     # ... other url patterns
-    path('create/,'create_campaign,name='create_campaign'),
+    path('create/', create_campaign, name='create_campaign'),
     path('update_campaign/', update_campaign, name='update_campaign'),
 ]
