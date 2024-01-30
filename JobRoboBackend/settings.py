36790bbb8b3e19bd0f14d2b58505db9eb56b12df
tarunkota <tarunkota4@gmail.com>
2024-01-30 16:35:05 +0530

    host bug fix

--- JobRoboBackend/settings.py
+++ JobRoboBackend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-8=!ds1)(^$*jw645^#j!l+^crzgr-tnn4@oflf(j48u^l@oelz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "jobrobo.ai/",
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "jobrobo.ai",
                  "3.142.79.103", "18.218.180.246", "3.145.41.95"]
 
 
